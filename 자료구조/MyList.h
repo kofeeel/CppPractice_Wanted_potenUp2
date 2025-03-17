9284919c6c8fa8ce6d05f6a2bf3198c0db64f75c
Hasimu <70192238+kofeeel@users.noreply.github.com>
2025-03-17 23:31:27 +0900

    Update MyList.h
    mergesort 추가

--- 자료구조/MyList.h
+++ 자료구조/MyList.h
@@ -17,7 +17,7 @@ public:
     만들어 놓은 연결리스트는. 추후 정렬 구현에도 사용 할 예정.
 
 */
-class MyList 
+class MyList
 {
 
 private:
@@ -26,33 +26,28 @@ private:
     int count;      //리스트에 저장된 노드 개수
 
     MyNode* atNode(int at);
-
+    MyNode* mergeSortRecursive(MyNode* head);
+    MyNode* merge(MyNode* left, MyNode* right);
+    MyNode* getMiddle(MyNode* head);
 public:
     MyList();
     ~MyList();
 
     void push_front(int val);
     void push_back(int val);
-
     void pop_back();
     void pop_front();
-
     int& front();    // 맨 앞의 값 반환.
     int& back();        // 맨 뒤의 값 반환.
     int at(int index);
-
     void insert(int index, int value);
     void erase(int index, int value);
     void clear();    // 리스트 내부의 모든 노드 삭제
-
     void print();    // 리스트 값들을 cout으로 출력(연습용 함수)
     size_t size();    // 리스트에 들어있는 데이터의 수 반환.
-
-    void BubbleSort(); 
-    void MergeRight(); //MergeSort() head ~ middle - 1
-    void MergeLeft();  //MergeSort() middle  ~ tail 
-    void MergeSort();
-    void Swap(int& a ,int& b);
+    void Swap(int& a, int& b);
     int Max(int x, int y);
-};
 
+    void BubbleSort();
+    void mergeSort();    
+};
